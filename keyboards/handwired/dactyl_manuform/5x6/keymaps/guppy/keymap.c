d30059cb5f010ed32f7ac62949a269f71e57be26
Karl Wang <karlwang@twitch.tv>
2019-10-14 22:57:40 -0700

    Added manuform keymap

---
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/guppy/keymap.c
@@ -0,0 +1,52 @@
+/* A standard layout for the Dactyl Manuform 5x6 Keyboard */ 
+
+#include QMK_KEYBOARD_H
+
+extern keymap_config_t keymap_config;
+
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT_5x6(
+     KC_ESC  , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                        KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+     KC_GRAVE, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                        KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLASH,
+     KC_TAB  , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                        KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+     KC_LSFT , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                        KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
+                      KC_LBRC,KC_RBRC,                                                       KC_MINS, KC_EQL,
+                                      RAISE,   KC_SPC,                        KC_ENT , LOWER,
+                                      KC_LGUI, KC_LCTL,                       KC_RCTL, KC_RGUI, 
+                                      KC_LALT, KC_HOME,                       KC_END , KC_RALT
+  ),
+
+  [_LOWER] = LAYOUT_5x6(
+
+     KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                         KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
+     _______,_______,_______,_______,_______,_______,                         _______,_______,_______,_______,_______,_______,
+     KC_HOME,LCTL(KC_LEFT),KC_UP,LCTL(KC_RIGHT),KC_END,KC_PGUP,               KC_HOME,LCTL(KC_LEFT),KC_UP,LCTL(KC_RIGHT),KC_END,KC_PGUP,
+     LALT(KC_LEFT),KC_LEFT,KC_DOWN,KC_RGHT,LALT(KC_RIGHT),KC_PGDN,            LALT(KC_LEFT), KC_LEFT, KC_DOWN, KC_RGHT, LALT(KC_RIGHT),KC_PGDN,
+                                             _______,_______,                 _______, _______,
+                                             _______,_______,                 _______,_______,
+                                             _______,_______,                 _______,_______,
+                                             _______,_______,                 RESET,_______
+
+  ),
+
+  [_RAISE] = LAYOUT_5x6(
+       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
+       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+       _______,KC_UNDO,KC_CUT ,KC_COPY,KC_PASTE,_______,                       _______,_______,_______,_______,_______,_______,
+                                            _______,_______,            _______,_______,
+                                            _______,_______,            _______,_______,
+                                            _______,_______,            _______,_______,
+                                            _______,_______,            _______,_______
+  ),
+};
+
+
