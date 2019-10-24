03d18beec2cf1e6ca19d353cf6210f5b27a55c0c
kaarukun <mythran@gmail.com>
2019-10-23 21:06:32 -0700

    added hex

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/v2/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/v2/keymap.c
@@ -18,10 +18,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                        KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLASH,
     KC_TAB,  KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                        KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                        KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
-                            KC_LCTL,KC_LGUI,                                               KC_HOME,KC_END,
+                            KC_LCTL,KC_LGUI,                                               KC_MINS, KC_EQL,
                                         KC_LALT,KC_SPC,                   KC_ENT, LOWER,
-                                            KC_LBRC,KC_UP,            KC_RGHT, KC_MINS,
-                                            KC_RBRC,KC_DOWN,          KC_LEFT, KC_EQL
+                                            KC_LBRC,KC_HOME,            KC_RGHT,KC_DOWN, 
+                                            KC_RBRC,KC_END,             KC_LEFT,KC_UP
   ),
 
   [_LOWER] = LAYOUT_5x6(
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,_______,_______,_______,_______,_______,                        KC_HOME,XXXXXXX,KC_UP,XXXXXXX,KC_END,XXXXXXX,
     _______,_______,_______,_______,_______,_______,                        RALT(KC_LEFT),KC_LEFT,KC_DOWN,KC_RGHT,RALT(KC_RGHT),XXXXXXX,
     _______,_______,_______,_______,_______,_______,                        KC_PGUP,KC_PGDN,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-                            MOD_MEH,MOD_HYPR,                                               XXXXXXX,XXXXXXX,
+                            _______,_______,                                               MOD_MEH,MOD_HYPR,
                                         RAISE,_______,                  _______,_______,
                                              _______,_______,         AG_TOGG,_______,
                                              _______,_______,         RESET,_______
